0fe50ff84f7840176af6b68d17b22a54c97db33b
21010628 <21010628@172.29.84.153>
2022-06-01 12:30:36 +0800

    first push to P00 repository in Github

---
+++ P00/src/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+
+	public int add (int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract (int c, int d) {
+		return c - d;
+	}
+	
+	public int multiple (int e, int f) {
+		return e * f;
+	}
+	
+	public int divide (int g, int h) {
+		if (h == 0) {
+			throw new IllegalArgumentException("Denominator cannot be zero");
+		}
+			return g / h;
+	}
+}
---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,91 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	private int a,b,c;
+	Calculator cal;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	
+	cal = new Calculator();
+	 
+	 a = 50;
+	 b = 50;
+	 c = 0;
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		//fail("Not yet implemented");
+		
+		int actual = cal.add(a, b);
+		int expected = 100;
+		
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void testSubtract() {
+		//fail("Not yet implemented");
+		
+		int actual = cal.subtract(a, b);
+		int expected = 0;
+		
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void testMultiple() {
+		//fail("Not yet implemented");
+		
+		int actual = cal.multiple(a, b);
+		int expected = 2500;
+		
+		assertEquals(actual, expected);
+	}
+	
+	@Test
+	public void testDivideByZero() {
+		//fail("Not yet implemented");
+		
+		try {
+			cal.divide(a,c);
+			fail("Expected an IllegalArgumentExeception to be thrown");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Denominator cannot be zero", e.getMessage());
+			} catch (Throwable t) {
+			assertEquals("Expected an IllegalArgumentExecption to be thrown", t.getMessage());
+		}
+	
+	}
+	
+	@Test
+	public void testDivide() {
+		//fail("Not yet implemented");
+		
+		int actual = cal.divide(a, b);
+		int expected = 1;
+		
+		assertEquals(actual, expected);
+	}
+
+}
